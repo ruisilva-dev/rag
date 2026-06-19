74488fc14a9f36bdf045af4a169bf35a80c4135d
Rui Silva <ruisilva@c1r7s7.42porto.com>
2026-06-19 16:47:19 +0100

    feat: implement type-safe Pydantic data models

---
+++ src/rag/models.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, Field
+import uuid
+
+
+class MinimalSource(BaseModel):
+    file_path: str
+    first_character_index: int
+    last_character_index: int
+
+
+class UnansweredQuestion(BaseModel):
+    question_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    question: str
+
+
+class AnsweredQuestion(UnansweredQuestion):
+    sources: list[MinimalSource]
+    answer: str
+
+
+class RagDataset(BaseModel):
+    rag_questions: list[AnsweredQuestion | UnansweredQuestion]
+
+
+class MinimalSearchResults(BaseModel):
+    question_id: str
+    question: str = Field(serialization_alias="question_str")
+    retrieved_sources: list[MinimalSource]
+
+
+class MinimalAnswer(MinimalSearchResults):
+    answer: str
+
+
+class StudentSearchResults(BaseModel):
+    search_results: list[MinimalSearchResults]
+    k: int
+
+
+class StudentSearchResultsAndAnswer(BaseModel):
+    search_results: list[MinimalAnswer]
+    k: int
